87acfffaaba135fbe64e26ce3f531bfcf0d648ee
Manshay <manshayverma4@gmail.com>
2026-07-27 19:07:41 +0530

    resolve some error

--- app/utils/citations.py
+++ app/utils/citations.py
@@ -7,7 +7,7 @@ def append_citations(
     grouped_source = defaultdict(set)
     for chunk in retrieved_chunks:
         metadata = chunk.get("metadata",{})
-        source = metadata.get("source","Unkown")
+        source = metadata.get("source","Unknown")
         chunk_id = metadata.get("chunk")
 
         if chunk_id is not None:
@@ -18,18 +18,18 @@ def append_citations(
     if not grouped_source:
         return answer
 
-    citation_lines = ["\n\nSource:"]
+    citation_lines = ["\n\nSources:"]
     for source, chunk_ids in grouped_source.items():
         chunk_ids= sorted(chunk_ids)
 
         if len(chunk_ids) == 1:
             citation_lines.append(
-                f"-{source}(Chunk:{chunk_ids[0]})"
+                f"- {source} (Chunk: {chunk_ids[0]})"
             )
         else:
-            chunks = ",".join(map(str,chunk_ids))
+            chunks = ", ".join(map(str,chunk_ids))
             citation_lines.append(
-                f"-{source}(Chunks:{chunks})"
+                f"- {source} (Chunks: {chunks})"
             )
     return answer + "\n" + "\n".join(citation_lines)
 
